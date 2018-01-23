4474d47c7c0401b737b05701fb3043d8582dbbd9
AnnKrn <anakrn.carreon@gmail.com>
2018-01-23 10:07:38 -0600

    cambio de vistas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function() {
+    // alert('hola')
+    setTimeout(function() {
+    location.href = "views/principal.html"; 
+    }, 3000);
+})
--- js/vendors/splash.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // alert('hola')
-    setTimeout(function() {
-    location.href = "../index.html"; 
-    }, 3000);
-})
